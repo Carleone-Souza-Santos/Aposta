e8476a3a7a716640a2af6fee6bbff065319a4a07
Carleone-Souza-Santos <carlleoneribeiro@hotmail.com>
2023-12-13 21:21:23 -0300

    adicionando regras de negocios

--- javascri.js
+++ javascri.js
@@ -8,6 +8,16 @@ function processing(){
    CollectValue.style.color = "#31FF4E";
    CollectValue.innerHTML=`O Número Sorteado foi ${result} Parabéns`
 
+if(valuemin > valuemax || valuemin == valuemax ){
+  document.querySelector('#valuemin').value ="";
+  document.querySelector('#valuemax').value ="";
+  CollectValue.innerHTML=`Não pode colocar valor maior no primeiro preenchimento ou igual!`
+  CollectValue.style.color = "#f00c0c";
+}
+if(valuemax > 100){
+  CollectValue.style.color = "#f00c0c";
+     CollectValue.innerHTML="max valor permitido e 100";
+}
   if(valuemin=="" || valuemax==""){
     CollectValue.style.color = "#f00c0c";
     CollectValue.innerHTML=`Vazio!!!!`
