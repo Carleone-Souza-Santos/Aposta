1065eb945c3e651b6d4d5d4b954939c927fd0a3b
Carleone-Souza-Santos <carlleoneribeiro@hotmail.com>
2023-12-13 22:06:10 -0300

    regras

--- javascri.js
+++ javascri.js
@@ -8,26 +8,37 @@ function processing(){
    CollectValue.style.color = "#31FF4E";
    CollectValue.innerHTML=`O Número Sorteado foi ${result} Parabéns`
 
+
+
+
+  //  regras de negocio.
+
+   // se o primeiro input for maior que o 2 ou igual 
 if(valuemin >= valuemax  ){
   document.querySelector('#valuemin').value ="";
   document.querySelector('#valuemax').value ="";
   CollectValue.innerHTML=`Não pode colocar valor maior no primeiro input ou igual!`
   CollectValue.style.color = "#f00c0c";
 }
+// valor max a receber aplicado no segundo input
 if(valuemax > 100){
   CollectValue.style.color = "#f00c0c";
      CollectValue.innerHTML="max valor permitido e 100";
 }
+
+// se os inputs forem vazios
   if(valuemin=="" || valuemax==""){
     CollectValue.style.color = "#f00c0c";
     CollectValue.innerHTML=`Vazio!!!!`
   }
-
-  if(valuemin < 0){
+// se os inputs forem menor ou igual a zero
+  if(valuemin <= 0){
     CollectValue.style.color = "#f00c0c";
     CollectValue.innerHTML=`Numero Negativos !!!!`
   }
-if(valuemax < 0){
+
+// se os inputs forem menor ou igual a zero
+if(valuemax <= 0){
     CollectValue.style.color = "#f00c0c";
     CollectValue.innerHTML=`Numero Negativos !!!!`
   }
