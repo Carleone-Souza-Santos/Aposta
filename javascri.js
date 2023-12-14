7631a79adb6f6c6f83e0d6171d754869a7f362b1
Carleone-Souza-Santos <carlleoneribeiro@hotmail.com>
2023-12-13 21:59:32 -0300

    alterando regra

--- javascri.js
+++ javascri.js
@@ -8,10 +8,10 @@ function processing(){
    CollectValue.style.color = "#31FF4E";
    CollectValue.innerHTML=`O Número Sorteado foi ${result} Parabéns`
 
-if(valuemin > valuemax || valuemin == valuemax ){
+if(valuemin >= valuemax  ){
   document.querySelector('#valuemin').value ="";
   document.querySelector('#valuemax').value ="";
-  CollectValue.innerHTML=`Não pode colocar valor maior no primeiro preenchimento ou igual!`
+  CollectValue.innerHTML=`Não pode colocar valor maior no primeiro input ou igual!`
   CollectValue.style.color = "#f00c0c";
 }
 if(valuemax > 100){
